cd86bca12cc4e405d1a54c96ea160ac66c77df1f
Ailina0728 <baiyue7@gmail.com>
2021-04-03 23:43:18 +0800

    Update lean-openwrt.sh

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.33/g' package/base-files/files/bin/config_generate
 
 # Add luci-app-ssr-plus
 pushd package/lean
@@ -58,6 +58,33 @@ git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 rm -rf ../lean/luci-theme-argon
 
+# Add luci-theme-darkmatter
+git clone --depth=1 https://github.com/apollo-ng/luci-theme-darkmatter
+
+# Add luci-theme-infinityfreedom
+git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
+
+# Add luci-app-advancedsetting
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages/trunk/luci-app-advancedsetting
+
+# Add luci-app-smartinfo
+svn co https://github.com/281677160/openwrt-package/trunk/luci-app-smartinfo
+
+# Add luci-app-dnsfilter
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages/trunk/luci-app-dnsfilter
+
+# Add luci-theme-opentomcat
+svn co https://github.com/281677160/openwrt-package/trunk/luci-theme-opentomcat
+
+# Add luci-theme-opentopd
+svn co https://github.com/281677160/openwrt-package/trunk/luci-theme-opentopd
+
+# Add luci-theme-rosy
+svn co https://github.com/281677160/openwrt-package/trunk/luci-theme-rosy
+
+# Add luci-app-ttnode
+svn co https://github.com/281677160/openwrt-package/trunk/luci-app-ttnode
+
 # Add subconverter
 git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
 
@@ -71,8 +98,8 @@ git clone --depth=1 https://github.com/destan19/OpenAppFilter
 git clone --depth=1 https://github.com/NateLol/luci-app-oled
 
 # Add driver for rtl8821cu & rtl8812au-ac
-svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8812au-ac
-svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8821cu
+#svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8812au-ac
+#svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8821cu
 popd
 
 # Mod zzz-default-settings
@@ -81,6 +108,15 @@ sed -i '/http/d' zzz-default-settings
 export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
 sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" zzz-default-settings
 popd
+
+# 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-X'' package/lean/default-settings/files/zzz-default-settings
+
+# 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
+sed -i "s/OpenWrt /M-Tea build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # Use Lienol's https-dns-proxy package
 pushd feeds/packages/net
@@ -95,9 +131,9 @@ svn co https://github.com/openwrt/packages/trunk/utils/syncthing
 popd
 
 # Fix mt76 wireless driver
-pushd package/kernel/mt76
-sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' Makefile
-popd
+#pushd package/kernel/mt76
+#sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' Makefile
+#popd
 
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
