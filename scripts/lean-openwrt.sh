d58a4fb98fc0b2344434e383df0bc494a5f3aa6e
Ailina0728 <baiyue7@gmail.com>
2021-04-10 20:05:26 +0800

    Update lean-openwrt.sh

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -123,9 +123,6 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-X'' pac
 # 版本号里显示一个自己的名字（xxxxxxx build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/OpenWrt /M-Tea build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
-# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
-
 # Use Lienol's https-dns-proxy package
 pushd feeds/packages/net
 rm -rf https-dns-proxy
